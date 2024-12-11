93a4359b6034f5e2d3a76ff1b3418c78f512a3f0
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-12-11 22:21:11 +0100

    refactor: remove Router wrapper from root render

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
