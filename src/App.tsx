b005875b5432f96e6f627c67b186123b966cd0c0
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-12-11 22:22:34 +0100

    refactor(app): move route configuration to a separate AppRoutes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Home, Contact, Service } from './pages';
-import { Banner, NavBar, NotFound } from './components';
+import AppRoutes from './routes/AppRoutes';
 import './style.css';
-import { BookLayoutRoutes } from './routes';
 
 const App: React.FC = () => {
-  return (
-    <React.Fragment>
-      <header>
-        <NavBar />
-        <section>
-          <Routes>
-            <Route path="/" element={<Banner />} />
-          </Routes>
-        </section>
-      </header>
-
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/books/*" element={<BookLayoutRoutes />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-    </React.Fragment>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
