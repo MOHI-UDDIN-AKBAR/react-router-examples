49df74908e19714501c3fa71297c002606f82f24
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-12-12 19:54:56 +0100

    feat(data): add productCategories list

---
+++ src/data/categories/product-categories.ts
@@ -0,0 +1,17 @@
+export const productCategories = [
+  {
+    id: 1,
+    name: 'Electronics',
+    description: 'Latest gadgets and electronics.',
+  },
+  {
+    id: 2,
+    name: 'Clothing',
+    description: 'Fashionable clothing for men and women.',
+  },
+  {
+    id: 3,
+    name: 'Books',
+    description: 'Explore a wide collection of books.',
+  },
+];
