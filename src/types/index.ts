d0d04d7027a6d2417f7c10368f5b56052a7d1d45
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-12-19 19:49:38 +0100

    chore(types): add ProductType and CategoryType for product and category data

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type ProductType = {
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  description: string;
+  details: string;
+};
+
+export type CategoryType = {
+  id: number;
+  name: string;
+};
